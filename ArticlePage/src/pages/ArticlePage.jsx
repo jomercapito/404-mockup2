4a928c6170d6a394ee0bf5f0072e85cd9cbe6c74
Kurt <russelnite.gmail.com>
2025-05-28 00:25:37 +0800

    UPDATE SCAFFOLD | Replace the App.jsx file; located inside the pages folder.

---
+++ ArticlePage/src/pages/ArticlePage.jsx
@@ -0,0 +1,12 @@
+import Footer from "../sections/Footer";
+
+function ArticlePage() {
+  return (
+    <>
+      // TODO: Navbar, Hero Section, Article Section
+      <Footer />
+    </>
+  );
+}
+
+export default ArticlePage;
