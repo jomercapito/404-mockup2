ea2f62832da03d2c3aa4b20b2dcc3be7cbed653b
Kurt <russelnite.gmail.com>
2025-05-28 00:26:44 +0800

    UPDATE SCAFFOLD | Replace the App.jsx import to ArticlePage.jsx.

--- ArticlePage/src/main.jsx
+++ ArticlePage/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import ArticlePage from "./pages/ArticlePage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ArticlePage />
   </StrictMode>
 );
