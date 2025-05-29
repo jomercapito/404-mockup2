950ff85d259ce3019dbbd6b1a2060ae647e33b2b
jomercapito <jomercapito1998@gmail.com>
2025-05-29 21:46:48 +0800

    Reapply "Initial commit for Hero component"
    This reverts commit 9f08ca0c6db56b8acf438a1098201f15a125857a.

---
+++ src/pages/ArticlePage.jsx
@@ -0,0 +1,14 @@
+import InterviewPage from "../sections/Hero";
+import Footer from "../sections/Footer";
+import HeroContent from "../sections/HeroContent";
+
+function ArticlePage() {
+  return (
+    <>
+      <InterviewPage />
+      <HeroContent />
+      <Footer />
+    </>
+  );
+}
+export default ArticlePage;
---
+++ src/sections/HeroContent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Hero.css";
+
+export default function HeroContent() {
+  return (
+    <>
+      <div className="hero-content">
+        <h1 className="hero-title">Interview with the Beatles</h1>
+        <p className="hero-description">
+          1-on-1 interview with the legendary band and an inside look at their
+          journey through out the years.
+        </p>
+      </div>
+    </>
+  );
+}
