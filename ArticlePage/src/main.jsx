2a983a6d3164e4a3e56b7c5a26c838fcaba8b97b
jomercapito <jomercapito1998@gmail.com>
2025-06-01 13:38:54 +0800

    disregards

--- ArticlePage/src/main.jsx
+++ ArticlePage/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import ArticlePage from "./pages/ArticlePage.jsx";
+import IndividualPage from "./pages/IndividualPage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ArticlePage />
+    <IndividualPage />
+    {/* <ArticlePage /> */}
   </StrictMode>
 );
