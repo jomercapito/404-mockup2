e6d2b2c17f19eff36ff387f269997e0d14708ed8
jomercapito <jomercapito1998@gmail.com>
2025-06-03 00:30:31 +0800

    Improved responsive layout and correct image paths.

--- ArticlePage/src/sections/navbar.jsx
+++ ArticlePage/src/sections/navbar.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { Menu, User } from "lucide-react";
 import "./navbar.css";
-import GroupLogo from "../assets/images/logos/Group.png";
+import groupLogo from "../assets/images/logos/Group.png";
+import rcaLogo from "../assets/images/logos/rca-logo.png";
 
 const Navbar = () => {
   return (
     <>
-      <div className="hero-image">
-        <nav className="navbar">
-          <div className="logo">
-            <img src="/rca-logo.png" alt="Logo" />
-          </div>
-          <div className="event-card">
-            <img src={GroupLogo} className="event-icon" />
+      <nav className="navbar">
+        <ul>
+          <li>
+            <img src={rcaLogo} alt="Logo" />
+          </li>
+          <li>
+            <img src={groupLogo} alt="Logo" className="event-icon" />
             <span className="event-label">Events</span>
-          </div>
-          <div className="menu-user-button">
+          </li>
+          <li className="menu-user-button">
             <Menu class="icon" />
             <User class="icon" />
-          </div>
-        </nav>
-      </div>
+          </li>
+        </ul>
+      </nav>
+      {/* </div> */}
     </>
   );
 };
