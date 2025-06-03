87a921f513e70ad95ac1ce7a5ca138999ec86ce4
jomercapito <jomercapito1998@gmail.com>
2025-06-03 23:52:23 +0800

    adding author and publishing date

--- ArticlePage/src/sections/Individual.jsx
+++ ArticlePage/src/sections/Individual.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import "./Hero.css";
+import Ellipse from "../assets/images/logos/Ellipse.svg";
 
 const categories = ["Theater", "Musical", "Comedy"];
 
@@ -17,6 +17,20 @@ export default function Individual() {
           </div>
         </main>
       </header>
+      <div className="author-card">
+        <div className="author-left">
+          <img src={Ellipse} alt="James Johnson" className="author-avatar" />
+          <div>
+            <p className="author-name">James Johnson</p>
+            <p className="author-title">Editor at Red Curtain Addict</p>
+          </div>
+          <button className="follow-button">Follow</button>
+        </div>
+        <div className="author-right">
+          <p>Published:</p>
+          <p>04/18/2021</p>
+        </div>
+      </div>
     </div>
   );
 }
