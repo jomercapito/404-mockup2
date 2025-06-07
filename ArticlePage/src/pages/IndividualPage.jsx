08fb42e2449d93ddefd885b906b33828ead8502e
Kurt <russelnite.gmail.com>
2025-06-08 02:09:46 +0800

    IMPORT | Import the Article Content Layout to be rendered in the Individual Page

--- ArticlePage/src/pages/IndividualPage.jsx
+++ ArticlePage/src/pages/IndividualPage.jsx
@@ -1,12 +1,14 @@
 import Footer from "../sections/Footer";
 import Individual from "../sections/Individual";
 import Navbar from "../sections/navbar";
+import ArticleContentLayout from "../components/IndividualArticleContentLayout";
 
 function IndividualPage() {
   return (
     <>
       <Navbar />
       <Individual />
+      <ArticleContentLayout />
       <Footer />
     </>
   );
